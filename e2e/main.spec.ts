bd00bd56007a4fe909c5d8aa69fde5858df10ad9
Tomasz Darmetko <isinlor@gmail.com>
2025-06-04 23:23:24 +0300

    Remove discourage use of networkidle.

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -3,6 +3,5 @@ import { test } from './test-setup';
 
 test('build success.', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.waitForLoadState('networkidle');
   await expect(page.locator('body')).toBeVisible();
 });
